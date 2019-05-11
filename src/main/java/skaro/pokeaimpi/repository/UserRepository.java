0f6a9c770bdbfced2a484b4e1d254a69a5edea3e
Ben Churchill <bencbchurchill@gmail.com>
2019-05-11 10:58:13 -0600

    Added some methods to the User JPA repository to get users by social connections instead of filtering at the service level

--- src/main/java/skaro/pokeaimpi/repository/UserRepository.java
+++ src/main/java/skaro/pokeaimpi/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package skaro.pokeaimpi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import skaro.pokeaimpi.repository.entities.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
+	Optional<UserEntity> findBySocialProfileDiscordConnectionDiscordId(Long discordId);
+	Optional<UserEntity> findBySocialProfileTwitchConnectionUserName(String userName);
 }
--- src/main/java/skaro/pokeaimpi/web/UserController.java
+++ src/main/java/skaro/pokeaimpi/web/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import skaro.pokeaimpi.services.UserService;
 import skaro.pokeaimpi.web.dtos.UserDTO;
 
 @Controller   
+@RequestMapping(path="/user")
 public class UserController {
 	
 	@Autowired
@@ -21,14 +23,19 @@ public class UserController {
 		
 	}
 	
-	@GetMapping(path="/user")
+	@GetMapping
 	public @ResponseBody List<UserDTO> getAll() {
 		return userService.getAll();
 	}
 	
-	@GetMapping("/user/discord/{id}")
+	@GetMapping("/discord/{id}")
 	public @ResponseBody UserDTO getByDiscordId(@PathVariable(value="id") Long id) {
 		return userService.getByDiscordId(id).orElse(null);
+	}
+	
+	@GetMapping("/twitch/{name}")
+	public @ResponseBody UserDTO getByTwitchName(@PathVariable(value="name") String name) {
+		return userService.getByTwitchName(name).orElse(null);
 	}
 	
 	
